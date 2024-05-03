1b32043b1c4e26d9147fd11505d9721a2054dc45
Sergey K <s4-7@yandex.ru>
2024-05-03 17:01:08 +0300

    20 task

---
+++ tasks19_20/src/main/java/tasks19_20/aspect/Aspect.java
@@ -0,0 +1,23 @@
+package tasks19_20.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@org.aspectj.lang.annotation.Aspect
+public class Aspect {
+    @Around("allServiceMethods()")
+    public Object executionTime(ProceedingJoinPoint point) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object object = point.proceed();
+        long endTime = System.currentTimeMillis();
+        log.info("Class Name: "+ point.getSignature().getDeclaringTypeName() +". Method Name: "+ point.getSignature().getName() + ". Time taken for Execution is : " + (endTime-startTime) +"ms");
+        return object;
+    }
+    @Pointcut("within(tasks19_20.services.*)")
+    public void allServiceMethods() {}
+}
